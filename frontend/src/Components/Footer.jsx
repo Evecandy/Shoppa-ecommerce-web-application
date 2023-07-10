a9558f7f283feede9cdb6a96b746b0a2ad8fe300
Evecandy <evecandymutunga@gmail.com>
2023-07-10 16:28:11 +0300

    Creates a footer

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -6,18 +6,25 @@ function Footer() {
 
     return (
       <>
-        <div className="footer">
+        <div className="footer-container">
+            <div className="footer">
             <div className="help">
-                
-            </div>
-            <div className="about">
-                
-            </div>
-            <div className="more">
-                
+                <p>Help and information</p>
+                <p>Help</p>
+                <p>Track your order</p> 
             </div>
 
-            
+            <div className="about">
+                <p>About Shoppa</p>
+                <p>About us</p>
+            </div>
+            <div className="more">
+                <p>More from Shoppa</p>
+                <p>Mobile and Shoppa apps</p>                
+            </div>
+            </div>
+
+           Created by Eve Candy Mutunga
             
         </div>
       </>
